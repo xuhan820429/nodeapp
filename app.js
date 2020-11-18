7f6e5b7e595da444e5d480f7194f186081494786
Xu Han <xuhan820429@gmail.com>
2020-11-17 19:14:43 -0500

    setup init server, add middleware (body-parser, cookie-parser, view engine, static folder),  main route

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.static("public"))
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log('server listen on port 3000')
+})
+
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+app.get("/content", (req, res) => {
+    res.render("content")
+})
